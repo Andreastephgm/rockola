f380048030b819f6914ee54e27204243993cc538
Andreastephgm <98714194+Andreastephgm@users.noreply.github.com>
2023-09-17 00:14:53 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MainStack from './.expo/Navigation/MainStack';
+import Welcome from './.expo/View/Welcome';
+
+export default function App() {
+  return (
+      <MainStack/>
+  );
+}
+
